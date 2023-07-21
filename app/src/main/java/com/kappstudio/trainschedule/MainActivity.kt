6618855b3e696b301d7bcb49e886b980b14abc13
AKuan <kuan31045@gmail.com>
2023-07-22 04:18:26 +0800

    Add theme color

--- app/src/main/java/com/kappstudio/trainschedule/MainActivity.kt
+++ app/src/main/java/com/kappstudio/trainschedule/MainActivity.kt
@@ -11,36 +11,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.kappstudio.trainschedule.ui.theme.TrainScheduleTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             TrainScheduleTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TrainScheduleTheme {
-        Greeting("Android")
     }
 }
